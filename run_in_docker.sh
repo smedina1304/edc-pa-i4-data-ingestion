6bb99378b505312a467c3f09b31e6d9435d84b75
Sérgio C. Medina <smedina1304@gmail.com>
2021-11-20 19:07:56 -0300

    Docker - KubernetesPodOperator - Jobs Python

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -6,22 +6,19 @@ echo "PRG_NAME: " $PRG_NAME
 cd /app
 
 echo "-> Secrets (copy)..."
-chown -R appuser /var/secrets
-ls -ls /var/secrets
 
-cat /var/secrets/credentials.json
-cat /var/secrets/key.json
-cat /var/secrets/settings.yaml
+ls -ls /var/secrets/credentials
+ls -ls /var/secrets/gcp
+ls -ls /var/secrets/settings
 
-mkdir /app/secrets
+# mkdir /app/secrets
 
-cp -R /var/secrets/credentials.json /app/secrets/credentials.json
-cp -R /var/secrets/key.json /app/secrets/key.json
-cp -R /var/secrets/settings.yaml /app/secrets/settings.yaml
+# cp -R /var/secrets/credentials.json /app/secrets/credentials.json
+# cp -R /var/secrets/key.json /app/secrets/key.json
+# cp -R /var/secrets/settings.yaml /app/secrets/settings.yaml
 
-ls -ls /app/secrets
-chmod -R 755 /app/secrets
-
+# ls -ls /app/secrets
+# chmod -R 755 /app/secrets
 
 echo "Executando a chamada..."
 pwd

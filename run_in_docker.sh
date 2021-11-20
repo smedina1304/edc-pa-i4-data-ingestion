ac7c119aebbc918d91efaf3f13dcf17fff98fbbd
Sérgio C. Medina <smedina1304@gmail.com>
2021-11-20 18:23:43 -0300

    Docker - KubernetesPodOperator - Jobs Python

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -5,6 +5,13 @@ echo "PRG_NAME: " $PRG_NAME
 
 cd /app
 
+echo "-> Secrets (copy)..."
+mkdir /app/secrets
+cp /var/secrets/*.* /app/secrets/*.* 
+ls -ls /app/secrets
+chmod -R 755 /app/secrets
+
+
 echo "Executando a chamada..."
 pwd
 python3 $PRG_NAME

53bfe286f7b0181841a1a9ce6b2854e0c0a098c8
Sérgio C. Medina <smedina1304@gmail.com>
2021-11-20 14:58:09 -0300

    Airflow DAGs - Ingestão de dados do Processo Ind.

--- airflow/dags/task-pa-i4-data-process.py
+++ airflow/dags/task-pa-i4-data-process.py
@@ -71,7 +71,7 @@ with DAG(
         'email_on_retry': False,
         'max_active_runs': 1,
     },
-    description='Processamento de dados de Produção - 1.2c',
+    description='Processamento de dados de Produção - 1.2d',
     schedule_interval="@once",
     start_date=airflow.utils.dates.days_ago(1),
     catchup=False,

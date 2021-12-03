bbce56b40c451e651b40b6c235b078551d321ecd
Sérgio C. Medina <smedina1304@gmail.com>
2021-12-03 14:24:27 -0300

    Pods - Airflow Job - Dataprod Cleaning

--- pods/job_dataprod_cleaning_preparation.py
+++ pods/job_dataprod_cleaning_preparation.py
@@ -124,63 +124,62 @@ if __name__ == "__main__":
         source = "dataprod"
         folder = f"raw-data-zone/{source}"
 
+        # Carregando os dados do arquivo da data e linha informados
+        df_dataprod = gcs.read_csv_to_df(folder=folder, dtexec=param_execution_date, lineprod=param_line_id, sep=";")
 
-# Carregando os dados do arquivo da data e linha informados
-df_dataprod = gcs.read_csv_to_df(folder=folder, dtexec=param_execution_date, lineprod=param_line_id, sep=";")
+        if df_dataprod is not None:
 
-if df_dataprod is not None:
+            # verificando se existe dados do dia posterior para completar o turno 3
+            nextDay = datetime.strptime(param_execution_date, "%Y-%m-%d") + timedelta(days=1)
+            print('DEBUG ->',"param_execution_date:", param_execution_date, " next:", nextDay)    
+            df = gcs.read_csv_to_df(folder=folder, dtexec=nextDay.strftime("%Y-%m-%d"), lineprod=param_line_id, sep=";")
+            if df is not None:
+                df_dataprod = df_dataprod.append(df, sort=False, ignore_index=True)
 
-    # verificando se existe dados do dia posterior para completar o turno 3
-    nextDay = datetime.strptime(param_execution_date, "%Y-%m-%d") + timedelta(days=1)
-    print('DEBUG ->',"param_execution_date:", param_execution_date, " next:", nextDay)    
-    df = gcs.read_csv_to_df(folder=folder, dtexec=nextDay.strftime("%Y-%m-%d"), lineprod=param_line_id, sep=";")
-    if df is not None:
-        df_dataprod = df_dataprod.append(df, sort=False, ignore_index=True)
+                # Limpa os dados nulos
+                df_dataprod.dropna(inplace=True)
 
-        # Limpa os dados nulos
-        df_dataprod.dropna(inplace=True)
+                # Alterando o delimitador decimal de "," para "."
+                df_dataprod['TOTMIN']=df_dataprod.TOTMIN.str.replace(',','.')
 
-        # Alterando o delimitador decimal de "," para "."
-        df_dataprod['TOTMIN']=df_dataprod.TOTMIN.str.replace(',','.')
+                # Define os tipos de cada coluna
+                df_dataprod['OP'] = df_dataprod['OP'].astype(str)
+                df_dataprod['LINE'] = df_dataprod['LINE'].astype(int)
+                df_dataprod['LINE'] = df_dataprod['LINE'].astype(str)
+                df_dataprod['TIMESTAMP'] = pd.to_datetime(df_dataprod['TIMESTAMP'])
+                df_dataprod['BATCH'] = df_dataprod['BATCH'].astype(str)
+                df_dataprod['TIMER'] = df_dataprod['TIMER'].astype(str)
+                df_dataprod['TOTMIN'] = df_dataprod['TOTMIN'].astype(float)
+                df_dataprod['STSID'] = df_dataprod['STSID'].astype(int)
+                df_dataprod['STSDS'] = df_dataprod['STSDS'].astype(str)
+                df_dataprod['PC'] = df_dataprod['PC'].astype(float)
+                df_dataprod['GOOD'] = df_dataprod['GOOD'].astype(float)
+                df_dataprod['REJECT'] = df_dataprod['REJECT'].astype(float)
 
-        # Define os tipos de cada coluna
-        df_dataprod['OP'] = df_dataprod['OP'].astype(str)
-        df_dataprod['LINE'] = df_dataprod['LINE'].astype(int)
-        df_dataprod['LINE'] = df_dataprod['LINE'].astype(str)
-        df_dataprod['TIMESTAMP'] = pd.to_datetime(df_dataprod['TIMESTAMP'])
-        df_dataprod['BATCH'] = df_dataprod['BATCH'].astype(str)
-        df_dataprod['TIMER'] = df_dataprod['TIMER'].astype(str)
-        df_dataprod['TOTMIN'] = df_dataprod['TOTMIN'].astype(float)
-        df_dataprod['STSID'] = df_dataprod['STSID'].astype(int)
-        df_dataprod['STSDS'] = df_dataprod['STSDS'].astype(str)
-        df_dataprod['PC'] = df_dataprod['PC'].astype(float)
-        df_dataprod['GOOD'] = df_dataprod['GOOD'].astype(float)
-        df_dataprod['REJECT'] = df_dataprod['REJECT'].astype(float)
+                # Definindo a Data de Produção DTPROD
+                df_dataprod.insert(0, 'DTPROD', None)
+                df_dataprod['DTPROD'] = df_dataprod.apply(lambda row:func.calc_dtprod(row['TIMESTAMP']),axis=1)
+                df_dataprod['DTPROD'] = df_dataprod['DTPROD'].astype(str)
+                
+                # Definindo a ID do Turno de Produção IDTURNO
+                df_dataprod.insert(1, 'IDTURNO', 0)
+                df_dataprod['IDTURNO'] = df_dataprod.apply(lambda row:func.calc_idturno(row['TIMESTAMP']),axis=1)
+                df_dataprod['IDTURNO'] = df_dataprod['IDTURNO'].astype(int)
 
-        # Definindo a Data de Produção DTPROD
-        df_dataprod.insert(0, 'DTPROD', None)
-        df_dataprod['DTPROD'] = df_dataprod.apply(lambda row:func.calc_dtprod(row['TIMESTAMP']),axis=1)
-        df_dataprod['DTPROD'] = df_dataprod['DTPROD'].astype(str)
-        
-        # Definindo a ID do Turno de Produção IDTURNO
-        df_dataprod.insert(1, 'IDTURNO', 0)
-        df_dataprod['IDTURNO'] = df_dataprod.apply(lambda row:func.calc_idturno(row['TIMESTAMP']),axis=1)
-        df_dataprod['IDTURNO'] = df_dataprod['IDTURNO'].astype(int)
+                # Seleciona apenas o periodo referente ao DTPROD
+                df_dataprod = df_dataprod.loc[ df_dataprod['DTPROD'] == param_execution_date ]
 
-        # Seleciona apenas o periodo referente ao DTPROD
-        df_dataprod = df_dataprod.loc[ df_dataprod['DTPROD'] == param_execution_date ]
+                # reindex
+                df_dataprod = df_dataprod.reset_index(drop=True)
 
-        # reindex
-        df_dataprod = df_dataprod.reset_index(drop=True)
+                # Verifica se existe arquivos no path para deletar
+                gcs.delete_blob(pathName=f"processing-zone/{source}/DTPROD={param_execution_date}")
 
-        # Verifica se existe arquivos no path para deletar
-        gcs.delete_blob(pathName=f"processing-zone/{source}/DTPROD={param_execution_date}")
+                # Gravando na Processing-Zone
+                gcs.write_pandas_to_parquet(path=f"edc-pa-i4-data/processing-zone/{source}", df=df_dataprod, partitionCols=['DTPROD'])
 
-        # Gravando na Processing-Zone
-        gcs.write_pandas_to_parquet(path=f"edc-pa-i4-data/processing-zone/{source}", df=df_dataprod, partitionCols=['DTPROD'])
+            # Se houve algum erro até este ponto é lançada uma exceção
+            if msg_error is not None:
+                raise Exception( msg_error )
 
-    # Se houve algum erro até este ponto é lançada uma exceção
-    if msg_error is not None:
-        raise Exception( msg_error )
-
-    print('\n')
+            print('\n')
